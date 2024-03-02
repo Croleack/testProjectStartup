237dd327de886165fbefd5d44e30ea6276ab20ac
Radik Khaziev <121854191+Croleack@users.noreply.github.com>
2024-03-03 00:46:42 +0500

    Initial Commit

---
+++ testProjectStartup/testProjectStartupApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectStartupApp.swift
+//  testProjectStartup
+//
+//  Created by Enzhe Gaysina on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct testProjectStartupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
